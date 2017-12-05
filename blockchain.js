5ae72fd1f71dacd0c03a1ba3bed6a28cd7bc7c2b
safal <safaldasg@gmail.com>
2017-12-05 17:25:30 +0530

    last hash function

--- blockchain.js
+++ blockchain.js
@@ -4,5 +4,10 @@ class BlockChain{
   constructor(){
     this.chain = [new Block(0,new Date(),'THE GENESIS BLOCK',0)]
   }
+
+  //get the hash of the last block
+  getLastHash(){
+    return this.chain[this.chain.length-1].hash
+  }
 }
 module.exports = BlockChain
