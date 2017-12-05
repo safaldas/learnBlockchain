cf50f800d471edca7509c69c201a3a0b71b1e836
safal <safaldasg@gmail.com>
2017-12-05 17:33:04 +0530

    Adding a block to the chain

--- blockchain.js
+++ blockchain.js
@@ -1,13 +1,27 @@
 const Block = require('./block')
 
-class BlockChain{
-  constructor(){
-    this.chain = [new Block(0,new Date(),'THE GENESIS BLOCK',0)]
+class BlockChain {
+  constructor() {
+    this.chain = [new Block(0, new Date(), 'THE GENESIS BLOCK', 0)]
   }
 
   //get the hash of the last block
-  getLastHash(){
-    return this.chain[this.chain.length-1].hash
+  getLastHash() {
+    return this.chain[this.chain.length - 1].hash
+  }
+
+  //Add a new block to the chain
+  addBlock(data) {
+    const index = this.chain.length
+    const timestamp = new Date().toUTCString()
+    const previousHash = this.getLastHash()
+    const newBlock = new Block(index, timestamp, data, previousHash)
+
+    if (this.isValidBlock(newBlock)) {
+      this.chain.push(newBlock)
+    } else {
+      console.error("The block is invalid and cannot be added.")
+    }
   }
 }
 module.exports = BlockChain
