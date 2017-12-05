d11687df94c6ef5bdf722bf06423c7ba86a88f3a
safal <safaldasg@gmail.com>
2017-12-05 17:17:58 +0530

    cretae a block and return hash

--- block.js
+++ block.js
@@ -1,7 +1,19 @@
 const crypto = require('crypto') //this is in the default node implementation
 
-class Block{
+class Block {
+  constructor(index, timestamp, data, previousHash) {
+    this.index = index
+    this.timestamp = timestamp
+    this.data = data
+    this.previousHash = previousHash
+    this.hash = this.calculateHash()
+  }
 
+  // creating the hash for the block
+  calculateHash() {
+    const string = this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)
+    return crypto.createHash('sha256').update('string').digest('hex')
+  }
 }
 
 modules.exports = Block
